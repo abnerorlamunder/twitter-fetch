202cef7b915c49e475a7c39233a5f8603a1ea770
Abner Orlamunder <orlamunder.abner@gmail.com>
2020-01-08 19:18:17 -0300

    versao final buscando tweets

--- twitterfetch/urls.py
+++ twitterfetch/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.hashtag_list, name='hashtag_list'),
+    path('hashtag_delete/<int:pk>', views.HashtagDelete.as_view(), name='hashtag_delete'),
 ]
--- twitterfetch/views.py
+++ twitterfetch/views.py
@@ -3,19 +3,43 @@ from django.utils import timezone
 from .models import Hashtag
 from .forms import HashtagForm
 from django.shortcuts import redirect
+from django.views.generic.edit import DeleteView
+import tweepy
+from django.urls import reverse_lazy
 
 # Create your views here.
 def hashtag_list(request):
   hashtags = Hashtag.objects.order_by('created_date')
+
+  auth = tweepy.OAuthHandler("IOBoByzw7sXqXe8zrSBL4Iyvq", "4DEC0VOGuevexfBTfy2bERIX8fRQtX5kGQPZ6Nhz3rxupgrb5c")
+  auth.set_access_token("1191824923301482501-Hr3h5O0QtIt7ONdMEvbPMMhRca4iuE", "1uf2tlpLCmQwwkT2zxVWcOtPTExSkm4TyjPvwqSYvR79y")
+
+  api = tweepy.API(auth)
+
+  hashtags_string = ""
+  for i in range(len(hashtags)):
+    if i != 0:
+      hashtags_string += ","
+    hashtags_string += hashtags[i].text
+
+  if hashtags_string != "":
+    tweets = api.search(hashtags_string, lang="en", rpp=100, result_type="recent", wait_on_rate_limit=True)
+  else:
+    tweets = []
+
   if request.method == "POST":
     form = HashtagForm(request.POST)
-    print(form)
     if form.is_valid():
       hashtag = form.save(commit=False)
       hashtag.save()
-      return render(request, 'hashtag_list.html', {'hashtags': hashtags})
+
+      return render(request, 'hashtag_list.html', {'hashtags': hashtags, 'tweets': tweets})
     else:
-      return render(request, 'hashtag_list.html', {'hashtags': hashtags})
+      return render(request, 'hashtag_list.html', {'hashtags': hashtags, 'tweets': tweets})
   else:
     form = HashtagForm()
-    return render(request, 'hashtag_list.html', {'hashtags': hashtags})
+    return render(request, 'hashtag_list.html', {'hashtags': hashtags, 'tweets': tweets})
+
+class HashtagDelete(DeleteView):
+    model = Hashtag
+    success_url = reverse_lazy('hashtag_list')
